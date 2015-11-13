9a99bb0cd02cae060caf965fd4ea45bd3ac165f5
Laura Kressin <laurakressin@gmail.com>
2015-11-13 10:32:28 -0600

    Outline for export CSV

--- public/javascripts/EmailController.js
+++ public/javascripts/EmailController.js
@@ -12,4 +12,26 @@ app.controller('EmailController', ['$scope', '$http', function($scope, $http) {
             return "No"
         }
     }
+
+    function getArray($scope.emails){
+        var headings = ["Timestamp", "Name", "Email", "Interested in Membership"];
+
+        var data = [];
+
+
+        for(var i = 0; i < $scope.emails.length; i++){
+            var newRow = {};
+
+            for(var j = 0; j < $scope.emails.length; j++){
+                newRow[headings[j]] = isNaN($scope.emails[i][headings[j]])?'"' + $scope.emails[i][headings[j]] + '"' : $scope.emails[i][headings[j]];
+            }
+        data.push(newRow);
+        }
+        console.log(data);
+        return data;
+
+
+    }
+
+
 }]);
--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,4 +1,4 @@
-var app = angular.module('dashboard', ['ngRoute']);
+var app = angular.module('dashboard', ['ngRoute', 'ngCsv']);
 
 app.config(function($routeProvider, $locationProvider){
     $routeProvider
