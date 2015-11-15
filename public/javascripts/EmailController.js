bdea5db3b30e4369170e07580221811c36d11001
schooolman <j.schoolmee@gmail.com>
2015-11-15 16:03:01 -0600

    moved export list into main content area, cleaned up code in controller

--- public/javascripts/EmailController.js
+++ public/javascripts/EmailController.js
@@ -48,20 +48,6 @@ app.controller('EmailController', ['$scope', '$http', function($scope, $http) {
 
     $scope.getArray = function() {
 
-        //var data = [];
-        //
-        //
-        //for(var i = 0; i < $scope.emails.length; i++){
-        //
-        //    var dataObj = {};
-        //
-        //    for(var j = 0; j < $scope.emails[i].length; j++){
-        //        dataObj.push($scope.emails[i][j])
-        //    }
-        //data.push(dataObj);
-        //}
-        //console.log(data);
-        //return data;
         return $scope.emails;
 
     };
