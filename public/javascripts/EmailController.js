628ea5bfd302f0f1e09009acf4e686c85436c953
Laura Kressin <laurakressin@gmail.com>
2015-11-16 10:04:36 -0600

    Getting all data for emails.

--- public/javascripts/EmailController.js
+++ public/javascripts/EmailController.js
@@ -11,9 +11,40 @@ app.controller('EmailController', ['$scope', '$http', function($scope, $http) {
         } else {
             return "No"
         }
-    }
+    };
 
-    var headings = ["Timestamp", "Name", "Email", "Interested in Membership"];
+    var headings = ["Timestamp", "Name", "Member", "Meeting with", "Email", "Twitter Handle", "Interested in Membership"];
+
+
+    $http.get('/data').then(function(res){
+
+
+        $scope.getSignIn = function(){
+            var signIns = [];
+
+            var data = [];
+
+            $scope.calledData = res.data;
+
+            for(var i = 0; i < $scope.calledData.length; i++){
+
+                var dataObj = {};
+
+                for(var j = 0; j < $scope.calledData[i].length; j++){
+                    dataObj.push($scope.calledData[i].temp_time, $scope.calledData[i].name, $scope.calledData[i].member, $scope.calledData[i].meeting_with, $scope.calledData[i].email, $scope.calledData[i].twitter)
+                    if($scope.calledData[i].email_me == 0){
+                        dataObj.push("No");
+                    } else {
+                        dataObj.push("Yes");
+                    }
+                }
+            data.push(dataObj);
+            }
+            console.log(data);
+            return data;
+        }
+    });
+
 
     $scope.getArray = function() {
 
