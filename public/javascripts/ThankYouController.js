ab448a70d5c520c1ead9eaa0af96c9157bc64112
Ryan M <rm.main@gmail.com>
2015-11-16 11:08:19 -0600

    Added css loading animation

--- public/javascripts/ThankYouController.js
+++ public/javascripts/ThankYouController.js
@@ -5,15 +5,6 @@ app.controller('ThankYouController', ['$scope', '$timeout', '$location', functio
 
     $scope.message = "Thank You Controller is Working";
 
-    //Adds in some loadings dots for UX before page load.
-    var dots = window.setInterval( function() {
-        var wait = document.getElementById("wait");
-        if ( wait.innerHTML.length > 2 )
-            wait.innerHTML = "";
-        else
-            wait.innerHTML += ".";
-    }, 500);
-
     //Changes the thanks page to index after 3 seconds.
     $timeout(function() {
         $location.path("/index")
