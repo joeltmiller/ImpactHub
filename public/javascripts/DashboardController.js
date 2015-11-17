79268509e99e95eb1fe598102697e35f11a81048
tongtonk <davidhoverson@yahoo.com>
2015-11-17 11:47:13 -0600

    updated user_pass login visable

--- public/javascripts/DashboardController.js
+++ public/javascripts/DashboardController.js
@@ -111,7 +111,7 @@ app.controller('DashboardController', ['$scope', '$http', function($scope, $http
 
         //$http({
         //    method:'JSONP',
-        //    url:"https://api.thedatabank.com/v1.0/login.asp?username=IMHSP_API&password=p8nRDaD2X0wc"
+        //    url:"https://api.thedatabank.com/v1.0/login.asp?username=*****&password=*****"
         //}).then(function(response) {
         //    console.log("Auth url and response.data", response.data);
         //}, function errorCallBack(response){
