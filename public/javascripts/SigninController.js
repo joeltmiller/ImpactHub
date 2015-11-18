14871026e8513801e2a9fc4409caf8982d16cd63
schooolman <j.schoolmee@gmail.com>
2015-11-18 09:59:59 -0600

    added password circles when typing in passcode.

--- public/javascripts/SigninController.js
+++ public/javascripts/SigninController.js
@@ -4,13 +4,19 @@ app.controller('SigninController', ['$scope', '$http', '$location', function($sc
     $scope.memberID = '';
     $scope.memberSuccess = '';
 
+    //Variables to show password circles as entered
+    $scope.passwordOne = false;
+    $scope.passwordTwo = false;
+    $scope.passwordThree = false;
+    $scope.passwordFour = false;
+
 
 
 
 
     $http({
         method:'JSONP',
-        url:"https://api.thedatabank.com/v1.0/login.asp?username=IMHSP_API&password=p8nRDaD2X0wc"
+        url:"https://api.thedatabank.com/v1.0/login.asp?username=&password="
     }).then(function(response) {
         console.log("Auth url and response.data", response.data);
     }, function errorCallBack(response){
@@ -23,6 +29,71 @@ app.controller('SigninController', ['$scope', '$http', '$location', function($sc
     $scope.keypadPress = function(value){
         console.log('pressing a button that is ', value);
         $scope.memberID +=  value;
+
+
+        //Shows password circles on keypad
+        switch ($scope.memberID.length){
+            case 1:
+                $scope.passwordOne = true;
+                break;
+            case 2:
+                $scope.passwordTwo = true;
+                break;
+            case 3:
+                $scope.passwordThree = true;
+                break;
+            case 4:
+                $scope.passwordFour = true;
+                break;
+            case 5:
+                alert('You have entered too many digits');
+                break;
+            default:
+                $scope.passwordOne = false;
+                $scope.passwordTwo = false;
+                $scope.passwordThree = false;
+                $scope.passwordFour = false;
+
+        }
+
+    };
+
+    //Deleting numbers on password field.
+    $scope.passwordDelete = function(){
+
+        var tempCode = $scope.memberID.slice(0, -1);
+        $scope.memberID = tempCode;
+        console.log($scope.memberID);
+
+
+        //Shows password circles on keypad
+        switch ($scope.memberID.length){
+            case 0:
+                $scope.passwordOne = false;
+                break;
+            case 1:
+                $scope.passwordTwo = false;
+                break;
+            case 2:
+                $scope.passwordThree = false;
+                break;
+            case 3:
+                $scope.passwordFour = false;
+                break;
+            case -1:
+                alert('Please enter code');
+                break;
+            default:
+                $scope.passwordOne = false;
+                $scope.passwordTwo = false;
+                $scope.passwordThree = false;
+                $scope.passwordFour = false;
+
+        }
+
+
+
+
     };
 
     $scope.submitCode = function(){
